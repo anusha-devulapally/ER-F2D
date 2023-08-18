798c498a9f55ded3207e41f96fcf71dfd16ab7c8
anusha-devulapally <64191013+anusha-devulapally@users.noreply.github.com>
2023-08-18 15:22:42 -0500

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -8,21 +8,7 @@ from torch.utils.data import ConcatDataset
 from data_loader.dataset import *
 #from data_loader_uni_norm.dataset import *
 import bisect
-""" DATASET Structure
-eventscape_dataset
-- train
-  -- Town01
-      -- sequence0
-          -- rgb
-          -- events
-          -- depth
-  -- Town02
-  -- Town03
-- val
-  -- Town05
-- test
-  -- Town05
-"""
+
 class ConcatDatasetCustom(ConcatDataset):
     def __getitem__(self, idx):
         if idx < 0:
@@ -48,10 +34,6 @@ def concatenate_subfolders(base_folder, dataset_type, event_folder, depth_folder
     print('Found {} samples in {}'.format(len(subfolders), base_folder))
 
     train_datasets = []
-    #for dataset_name in subfolders:
-      #print(dataset_name, len(os.listdir(base_folder+"/"+dataset_name)))
-        #for sequences in os.listdir(base_folder+"/"+dataset_name):
-          #print("sequences :", sequences)
     train_datasets.append(eval(dataset_type)(base_folder=base_folder, event_folder=event_folder,
                                                  depth_folder=depth_folder,
                                                  frame_folder=frame_folder, 
@@ -67,22 +49,16 @@ def concatenate_subfolders(base_folder, dataset_type, event_folder, depth_folder
                                                  use_phased_arch=use_phased_arch,
                                                  baseline=baseline,
                                                  reg_factor=reg_factor, recurrency=recurrency))
-          
-          #mean_stds.append(mean_std)
+
     if dataset_idx_flag == False:
         concat_dataset = ConcatDataset(train_datasets)
-        #means_concat = ConcatDataset(mean_stds)
     elif dataset_idx_flag == True:
         concat_dataset = ConcatDatasetCustom(train_datasets)
-        #means_concat = ConcatDatasetCustom(mean_stds)
     return concat_dataset#, means_concat
 
-event_path = "events/voxels"  # for accumulated events. To use voxels path = "events/voxel"
+event_path = "events/voxels"
 rgb_path = "rgb/davis"
 gt_path = "depth/data"
-#
-#def rgb2gray(rgb):
-#  return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140]).astype(np.float32)
             
 def build_dataset(set, transform, args):
    if(set=='train'):
