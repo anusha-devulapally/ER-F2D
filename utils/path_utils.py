6bc7fa9f66af57d744aefa1d86805fa75585d2bb
anusha-devulapally <64191013+anusha-devulapally@users.noreply.github.com>
2024-02-19 16:43:49 -0500

    Delete utils directory

--- utils/path_utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def ensure_dir(path):
-    try:
-      if not os.path.exists(path):
-        os.makedirs(path)
-    except FileExistsError:
-      print("folder {} already exists".format(path))
